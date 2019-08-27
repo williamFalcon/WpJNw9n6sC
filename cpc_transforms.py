28147953e044842acea620ac85fcec99bf544e28
William Falcon <waf2107@columbia.edu>
2019-08-27 13:58:32 -0400

    Create cpc_transforms.py

---
+++ cpc_transforms.py
@@ -0,0 +1,112 @@
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+from torch.nn.functional import unfold
+
+
+class CPCTransformsC10:
+    '''
+    Apply the same input transform twice, with independent randomness.
+    '''
+
+    def __init__(self):
+        # flipping image along vertical axis
+        self.flip_lr = transforms.RandomHorizontalFlip(p=0.5)
+        # image augmentation functions
+        normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                         std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
+        col_jitter = transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.2)], p=0.8)
+        img_jitter = transforms.RandomApply([
+            RandomTranslateWithReflect(4)], p=0.8)
+        rnd_gray = transforms.RandomGrayscale(p=0.25)
+        # main transform for self-supervised training
+        self.train_transform = transforms.Compose([
+            img_jitter,
+            col_jitter,
+            rnd_gray,
+            transforms.ToTensor(),
+            normalize,
+            Patchify(patch_size=8, overlap_size=4),
+        ])
+        # transform for testing
+        self.test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+            Patchify(patch_size=8, overlap_size=4),
+        ])
+
+    def __call__(self, inp):
+        inp = self.flip_lr(inp)
+        out1 = self.train_transform(inp)
+        return out1
+
+
+class RandomTranslateWithReflect:
+    '''
+    Translate image randomly
+    Translate vertically and horizontally by n pixels where
+    n is integer drawn uniformly independently for each axis
+    from [-max_translation, max_translation].
+    Fill the uncovered blank area with reflect padding.
+    '''
+
+    def __init__(self, max_translation):
+        self.max_translation = max_translation
+
+    def __call__(self, old_image):
+        xtranslation, ytranslation = np.random.randint(-self.max_translation,
+                                                       self.max_translation + 1,
+                                                       size=2)
+        xpad, ypad = abs(xtranslation), abs(ytranslation)
+        xsize, ysize = old_image.size
+
+        flipped_lr = old_image.transpose(Image.FLIP_LEFT_RIGHT)
+        flipped_tb = old_image.transpose(Image.FLIP_TOP_BOTTOM)
+        flipped_both = old_image.transpose(Image.ROTATE_180)
+
+        new_image = Image.new("RGB", (xsize + 2 * xpad, ysize + 2 * ypad))
+
+        new_image.paste(old_image, (xpad, ypad))
+
+        new_image.paste(flipped_lr, (xpad + xsize - 1, ypad))
+        new_image.paste(flipped_lr, (xpad - xsize + 1, ypad))
+
+        new_image.paste(flipped_tb, (xpad, ypad + ysize - 1))
+        new_image.paste(flipped_tb, (xpad, ypad - ysize + 1))
+
+        new_image.paste(flipped_both, (xpad - xsize + 1, ypad - ysize + 1))
+        new_image.paste(flipped_both, (xpad + xsize - 1, ypad - ysize + 1))
+        new_image.paste(flipped_both, (xpad - xsize + 1, ypad + ysize - 1))
+        new_image.paste(flipped_both, (xpad + xsize - 1, ypad + ysize - 1))
+
+        new_image = new_image.crop((xpad - xtranslation,
+                                    ypad - ytranslation,
+                                    xpad + xsize - xtranslation,
+                                    ypad + ysize - ytranslation))
+        return new_image
+
+    
+class Patchify(object):
+
+    def __init__(self, patch_size, overlap_size):
+        self.patch_size = patch_size
+        self.overlap_size = overlap_size
+
+    def __call__(self, x):
+        x = x.unsqueeze(0)
+        b, c, h, w = x.size()
+
+        # patch up the images
+        # (b, c, h, w) -> (b, c*patch_size, L)
+        x = unfold(x, kernel_size=self.patch_size, stride=self.overlap_size)
+
+        # (b, c*patch_size, L) -> (b, nb_patches, width, height)
+        x = x.transpose(2, 1).contiguous().view(b, -1, self.patch_size, self.patch_size)
+
+        # reshape to have (b x patches, c, h, w)
+        x = x.view(-1, c, self.patch_size, self.patch_size)
+
+        x = x.squeeze(0)
+
+        return x
